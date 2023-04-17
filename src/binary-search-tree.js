ca6278dad4f803a026491a6b962d5790cde0667d
Kasia Harai-Kamockaya <air@Kasias-Air.lan>
2023-04-17 10:32:41 -0400

    feat: add solution

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,34 +1,72 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
-
+  rootNode = null;
+  constructor() {
+    this.rootNode = null;
+  }
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootNode;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    let secondNode = new Node(data);
+    if (this.rootNode === null) {
+      this.rootNode = secondNode; 
+    } else {
+      this.insertNode(this.rootNode, secondNode)
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  insertNode(node, secondNode) {
+    if (secondNode.data < node.data) {
+      if (node.left === null) {
+        node.left = secondNode;
+      } else {
+        this.insertNode(node.left, secondNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = secondNode;
+      } else {
+        this.insertNode(node.right, secondNode);
+      }
+    }
+  }
+  
+
+  has(data) {
+    let result = this.find(data);
+    if (result != null) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    let newNode = this.rootNode;
+    while (newNode != null) {
+      if (data === newNode.data) {
+        return newNode;
+      }
+      if (data < newNode.data) {
+        newNode = newNode.left;
+      } else {
+        newNode = newNode.right;
+      }
+    }
+
+    return null;
   }
 
-  remove(/* data */) {
+  remove(data) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
